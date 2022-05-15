5b00dc2e8a3594cd096dd0a79a085d373c45513b
Tyler Vick <tvick@snapchat.com>
2022-05-15 11:14:00 -0600

    Initial Commit

---
+++ Shared/Better_HA_ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_HA_ControlsApp.swift
+//  Shared
+//
+//  Created by Tyler Vick on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Better_HA_ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
